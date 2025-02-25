5330873c820387446f55028c16e06e1374b05926
thachnguyensg <thachnn.sg@gmail.com>
2025-02-25 19:45:53 +0700

    and home page

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	const rootPath = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := &http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	fmt.Println("Server running at :8080")
-	server.ListenAndServe()
+
+	log.Printf("Server running at :%s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
