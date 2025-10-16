b47c9fa0f817a9d9d7820804a5e73a59c5240540
thachnguyensg <thachnn.sg@gmail.com>
2025-10-16 15:50:26 +0700

    add goose postgres schema

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
